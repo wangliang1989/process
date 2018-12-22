6f391da38d3ee482278e66ed893791e932f5531b
wangliang <wangliang0222@gmail.com>
2018-12-22 13:30:18 +0800

    add breq2fast

--- 4.synchronize.pl
+++ 4.synchronize.pl
@@ -8,48 +8,30 @@ my ($dir) = @ARGV;
 
 chdir $dir or die;
 
-my ($origin, $evla, $evlo, $evdp, $mag);
-my $yyyy = substr $dir, 0, 4;
-my $mm = substr $dir, 4, 2;
-my $dd = substr $dir, 6, 2;
-$origin = "${yyyy}-${mm}-${dd}T00:00:00.000";
-if (-e "event.info") {
-    open(INFO, "< event.info") or die "Cannot find event info.";
-    my $eventinfo = <INFO>;
-    ($origin, $evla, $evlo, $evdp, $mag) = split " ", $eventinfo;
-    close(INFO);
-}
-# 对发震时刻做解析
-my ($date, $time) = split "T", $origin;
-my ($year, $month, $day) = split "-", $date;
-my ($hour, $minute, $second) = split ":", $time;
-# 秒和毫秒均为整数
-my ($sec, $msec) = split /\./, $second;
-$msec = int(($second - $sec) * 1000 + 0.5);
-
-# 计算发震日期是一年中的第几天
+my $year = substr $dir, 0, 4;
+my $month = substr $dir, 4, 2;
+my $day = substr $dir, 6, 2;
+my $hour = "00";
+my $minute = "00";
+my $second = "00"
 my $jday = strftime("%j", $second, $minute, $hour, $day, $month-1, $year-1900);
 
-chdir $dir;
 open(SAC, "| sac") or die "Error in opening SAC\n";
 print SAC "wild echo off \n";
 print SAC "r *.SAC \n";
-print SAC "synchronize \n";   # 同步所有文件的参考时刻
+print SAC "synchronize \n";
 print SAC "ch o gmt $year $jday $hour $minute $sec $msec \n";
 print SAC "ch allt (0 - &1,o&) iztype IO \n";
-print SAC "ch evlo $evlo evla $evla evdp $evdp mag $mag \n" if (defined($evlo));
 print SAC "wh \n";
 print SAC "q \n";
 close(SAC);
 
-unless (defined($evlo)) {
-    open(SAC, "| sac") or die "Error in opening SAC\n";
-    print SAC "wild echo off \n";
-    print SAC "cuterr fillz \n";
-    print SAC "cut 0 86400 \n";
-    print SAC "r *.SAC \n";
-    print SAC "w over \n";
-    print SAC "q \n";
-    close(SAC);
-}
+open(SAC, "| sac") or die "Error in opening SAC\n";
+print SAC "wild echo off \n";
+print SAC "cuterr fillz \n";
+print SAC "cut 0 86400 \n";
+print SAC "r *.SAC \n";
+print SAC "w over \n";
+print SAC "q \n";
+close(SAC);
 chdir "..";
--- process.pl
+++ process.pl
@@ -2,20 +2,9 @@
 use strict;
 use warnings;
 
-@ARGV >= 1 or die "Usage: \n\tperl $0 event1 event2 ... eventn\n\tperl $0 events.info\n";
+@ARGV >= 1 or die "Usage: \n\tperl $0 dir1 dir2 ... dirn\n";
 
 my @events;
-
-if (@ARGV == 1 and -f $ARGV[0]) {
-    open(IN, "< $ARGV[0]");
-    foreach (<IN>) {
-        my ($event) = split /\s+/;
-        push @events, $event;
-    }
-    close(IN);
-} else {
-    @events = @ARGV;
-}
 
 foreach my $event (@events) {
     system "perl 2.merge.pl $event";
